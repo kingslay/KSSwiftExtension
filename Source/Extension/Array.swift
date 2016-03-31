0d6908bfcd6b546d0060ed05139895ff52845552
wangjinbian <kingslay@icloud.com>
2016-03-31 08:44:33 +0800

    no message

--- Source/Extension/Array.swift
+++ Source/Extension/Array.swift
@@ -8,10 +8,5 @@
 
 import Foundation
 extension Array {
-    public func forEach(iterator: (Element) -> Void) -> Array {
-        for item in self {
-            iterator(item)
-        }
-        return self;
-    }
+   
 }
