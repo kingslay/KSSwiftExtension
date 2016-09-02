d54c8d014d16a973c911094a33cba93205a7e47b
wangjinbian <kingslay@icloud.com>
2016-09-02 23:25:27 +0800

    no message

--- RxCocoa/Source/Extension/UIViewController+Rx.swift
+++ RxCocoa/Source/Extension/UIViewController+Rx.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 import RxCocoa
-import KSSwiftExtension
-
 public extension UIViewController {
     #if DEBUG
     #else
@@ -41,7 +39,6 @@ public extension UIViewController {
         }.addDisposableTo(self.ks.disposableBag)
     }
 }
-
 extension UIViewController: KSCompatible { }
 extension Swifty where Base: UIViewController {
     public func autoAdjustKeyBoard() {
